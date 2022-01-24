c2767867655a7f1123f5b870f6cb0121a983b1fa
faiz <faizsagheer4@gmail.com>
2022-01-24 21:47:27 +0500

    main url added

--- resumeproject/urls.py
+++ resumeproject/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #  main page url
+    path("",views.home, name='home'),
+
+    # contact page url
+    path('contact/',views.contact, name='contact'),
+
+    # services page url
+    path('serv/', include('serv.urls')),
+
+    # skill page url
+    path('edu/', include('edu.urls'))
 ]
